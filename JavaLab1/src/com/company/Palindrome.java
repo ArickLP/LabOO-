5e0ca44b82552db747dd090e73a836ca80853f1d
ArickLP <59561279+ArickLP@users.noreply.github.com>
2020-12-04 03:48:15 +0300

    Add files via upload

---
+++ JavaLab1/src/com/company/Palindrome.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Palindrome {  public static void main(String[] args) {
+
+    String[] s = {"java", "Palindrome", "apple", "racecar", "madam", "kayak", "moon", "song"};
+    for (int i = 0; i < s.length; i++) {
+        System.out.println("Введённое слово "+s [i] +" является палиндромом - " + isPalindrome(s[i]));
+    }
+    Scanner in = new Scanner(System.in);
+    System.out.print("Введите слово для проверки: ");
+    String str = in.next();
+    System.out.print("Введённое слово является палиндромом - " + isPalindrome(str));
+
+}
+    public static String reverseString(String s)
+    {
+        String reverse ="";
+        for (int i = s.length()-1; i >=0 ; i--) {
+            reverse+=s.charAt(i);
+        }
+        return reverse;
+    }
+    public static boolean isPalindrome(String s)
+    {
+        String reverse = reverseString(s);
+        return s.equals(reverse);
+    }
+}
+
